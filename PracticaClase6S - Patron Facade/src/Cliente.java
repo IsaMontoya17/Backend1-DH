3a397b40c190ae8a43d97c1f1728eb3ddaab0f8e
IsaMontoya17 <isabelamontoya2021@gmail.com>
2024-07-31 13:16:18 -0500

    casi terminado

--- PracticaClase6S - Patron Facade/src/Cliente.java
+++ PracticaClase6S - Patron Facade/src/Cliente.java
@@ -8,30 +8,20 @@ public class Cliente {
         List<Vuelo> vuelos = new ArrayList<>();
         vuelos.add(new Vuelo(LocalDate.of(2024, 8, 1), LocalDate.of(2024, 8, 15), "New York", "Los Angeles"));
         vuelos.add(new Vuelo(LocalDate.of(2024, 8, 5), LocalDate.of(2024, 8, 20), "London", "Paris"));
-        vuelos.add(new Vuelo(LocalDate.of(2024, 8, 10), LocalDate.of(2024, 8, 25), "Tokyo", "Osaka"));
+        vuelos.add(new Vuelo(LocalDate.of(2024, 8, 1), LocalDate.of(2024, 8, 25), "Tokyo", "Los Angeles"));
         vuelos.add(new Vuelo(LocalDate.of(2024, 8, 15), LocalDate.of(2024, 8, 30), "Berlin", "Munich"));
-        vuelos.add(new Vuelo(LocalDate.of(2024, 8, 20), LocalDate.of(2024, 9, 5), "Sydney", "Melbourne"));
-        vuelos.add(new Vuelo(LocalDate.of(2024, 8, 25), LocalDate.of(2024, 9, 10), "Toronto", "Vancouver"));
-        vuelos.add(new Vuelo(LocalDate.of(2024, 9, 1), LocalDate.of(2024, 9, 15), "Madrid", "Barcelona"));
-        vuelos.add(new Vuelo(LocalDate.of(2024, 9, 5), LocalDate.of(2024, 9, 20), "Rome", "Milan"));
-        vuelos.add(new Vuelo(LocalDate.of(2024, 9, 10), LocalDate.of(2024, 9, 25), "Beijing", "Shanghai"));
-        vuelos.add(new Vuelo(LocalDate.of(2024, 9, 15), LocalDate.of(2024, 9, 30), "Moscow", "Saint Petersburg"));
 
         List<Hotel> hoteles = new ArrayList<>();
-        hoteles.add(new Hotel(LocalDate.of(2024, 8, 1), LocalDate.of(2024, 8, 5), "New York"));
+        hoteles.add(new Hotel(LocalDate.of(2024, 8, 1), LocalDate.of(2024, 8, 5), "Los Angeles"));
         hoteles.add(new Hotel(LocalDate.of(2024, 8, 6), LocalDate.of(2024, 8, 10), "Los Angeles"));
         hoteles.add(new Hotel(LocalDate.of(2024, 8, 11), LocalDate.of(2024, 8, 15), "Paris"));
-        hoteles.add(new Hotel(LocalDate.of(2024, 8, 16), LocalDate.of(2024, 8, 20), "London"));
-        hoteles.add(new Hotel(LocalDate.of(2024, 8, 21), LocalDate.of(2024, 8, 25), "Tokyo"));
-        hoteles.add(new Hotel(LocalDate.of(2024, 8, 26), LocalDate.of(2024, 8, 30), "Osaka"));
-        hoteles.add(new Hotel(LocalDate.of(2024, 9, 1), LocalDate.of(2024, 9, 5), "Berlin"));
-        hoteles.add(new Hotel(LocalDate.of(2024, 9, 6), LocalDate.of(2024, 9, 10), "Munich"));
-        hoteles.add(new Hotel(LocalDate.of(2024, 9, 11), LocalDate.of(2024, 9, 15), "Sydney"));
-        hoteles.add(new Hotel(LocalDate.of(2024, 9, 16), LocalDate.of(2024, 9, 20), "Melbourne"));
+        hoteles.add(new Hotel(LocalDate.of(2024, 8, 1), LocalDate.of(2024, 8, 25), "Los Angeles"));
+
 
         String ciudadDestino = "Los Angeles";
         LocalDate fecha = LocalDate.of(2024, 8, 1);
         BusquedaImpl busqueda = new BusquedaImpl();
+
         busqueda.busquedaCompleta(ciudadDestino, fecha, vuelos, hoteles);
 
     }
