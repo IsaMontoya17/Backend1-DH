c1399f5bfaf0e230e4ed46ad1e7139766bb09c57
IsaMontoya17 <isabelamontoya2021@gmail.com>
2024-08-08 21:04:40 -0500

    clase 11 repaso dao

--- Clase 10 - Patron DAO/src/servicio/MedicamentoService.java
+++ Clase 10 - Patron DAO/src/servicio/MedicamentoService.java
@@ -11,18 +11,22 @@ public class MedicamentoService {
     private IDao<Medicamento> medicamentoIDao;
 
     public MedicamentoService() {
+
         this.medicamentoIDao = new MedicamentoDAOH2();
     }
 
     public Medicamento guardar(Medicamento medicamento) {
+
         return medicamentoIDao.guardar(medicamento);
     }
 
     public Medicamento buscarPorId(Integer id) {
+
         return medicamentoIDao.buscarPorId(id);
     }
 
     public List<Medicamento> listar() {
+
         return medicamentoIDao.listar();
     }
 
